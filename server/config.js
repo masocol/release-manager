0b3b8544243813c08cad248d6647159cec1b1576
Raymond Masocol <masocol@google.com>
2023-08-07 14:56:53 -0700

    check in

---
+++ server/config.js
@@ -0,0 +1,16 @@
+// config.js
+
+const config = {
+    development: {
+      serverUrl: 'localhost', // Replace this with your development server URL
+    },
+    uat: {
+      serverUrl: 'http://uat.example.com', // Replace this with your UAT server URL
+    },
+    production: {
+      serverUrl: 'http://44.217.174.50:5000', // Replace this with your production server URL
+    },
+  };
+  
+  export default config;
+  
